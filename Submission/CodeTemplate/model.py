510e5e36a8a6280ecde2558ca0492a05a83db16c
gsarafraz <gita.sarafraz@gmail.com>
2023-05-03 00:10:53 -0700

    Add files via upload

---
+++ Submission/CodeTemplate/model.py
@@ -0,0 +1,26 @@
+import pandas as pd
+
+
+class model():
+    
+    def __init__(self):
+        pass
+    
+    def evaluate(self, test_dataframe):
+        
+        '''
+            The input of this function is a pandas dataframe 
+            with the declared format mentioned in the file 
+            of each question and its output should be 
+            a dataframe with the specified format
+            
+        '''
+        
+        test_result = pd.DataFrame([1,2,3])
+        
+        return test_result
+        
+
+
+
+
